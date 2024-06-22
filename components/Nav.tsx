8f795cbc1d65e2f573ed2b98026890393f6d5b9f
Arrizki0098 <akbar.rabbani@iconpln.co.id>
2024-06-23 06:56:56 +0700

    Init Commit

---
+++ components/Nav.tsx
@@ -0,0 +1,34 @@
+import { ShoppingBagIcon } from '@heroicons/react/20/solid'
+import { HeartIcon } from '@heroicons/react/20/solid'
+import { UserIcon } from '@heroicons/react/20/solid'
+import { MagnifyingGlassCircleIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='h-[13vh] bg-gray-800'>
+            <div className='w-[95%] md:w-[80%] mx-auto h-[100%] flex items-center justify-between'>
+                {/* <div>
+                    <Image src="/images/logo.png" alt="logo" width={130} height={130} priority/>
+                </div> */}
+                <div className='h-[50%] hidden flex-[0.7] overflow-hidden bg-gray-200 rounded-md md:flex items-center'>
+                    <input type="text" placeholder='Search Games (Cth: Dota 2, Valorant)' className='block pl-[0.5rem] w-[90%] outline-none mx-auto h-[100%] bg-gray-200' />
+                    <MagnifyingGlassCircleIcon className='w-[1.8rem] h-[1.8rem] mr-[1.4rem] cursor-pointer' />
+                </div>
+                <div className='flex items-center justify-center space-x-8'>
+                    <div className='relative'>
+                        <ShoppingBagIcon className='w-[2rem] h-[2rem] text-white cursor-pointer' />
+                        <div className='w-[20px] text-[12px] absolute top-[-7px] right-[-7px] h-[20px] flex items-center text-white justify-center font-semibold rounded-full bg-red-400'>
+                            4
+                        </div>
+                    </div>
+                    <HeartIcon className='w-[2rem] h-[2rem] text-white cursor-pointer' />
+                    <UserIcon className='w-[2rem] h-[2rem] text-white cursor-pointer' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
